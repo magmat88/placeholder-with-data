087ee640877fc8392f2c9034c7ac619c6f716779
magmat88 <magdalena.matusiak88@gmail.com>
2022-02-16 19:16:07 +0100

    Added logic to loading data. Added handler to button.

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
-// let frenchMenData: Array<FrenchMen>;
-// let frenchMenAges: Array<number> = [];
 function getUsers() {
     return fetch('https://randomuser.me/api/?results=1000&gender=male&nat=fr&inc=name,location,dob,picture&noinfo')
-        .then(function (res) { return res.json(); })
-        .then(function (res) {
+        .then((res) => res.json())
+        .then((res) => {
         return res.results;
-        // frenchMenData = res.results;
-        // let frenchMen: FrenchMen;
-        // for (let frenchMen in frenchMenData) {
-        //     frenchMenAges.push((frenchMen.dob.age);
-        //     console.log(`${frenchMenData[0].name.first} ${frenchMenData[0].name.last}, age: ${frenchMenData[0].dob.age}, city: ${frenchMenData[0].location.city}`)
-        // }
     });
 }
-// const result: HTMLElement | null = document.getElementById('result');
-// const myResults = getUsers().then((users) => {
-//   for (let user in users) {
-//       console.log(user);
-//   }
-// });
 function compareAge(a, b) {
     if (a.dob.age < b.dob.age) {
         return -1;
@@ -28,26 +14,134 @@ function compareAge(a, b) {
     }
     return 0;
 }
-var tableResults = document.getElementById('tableResults');
-getUsers().then(function (users) {
+function sortByAge(users) {
     users.sort(compareAge);
-    for (var i = 999; i >= 990; i--) {
-        var newTableRow = document.createElement('tr');
-        var newDataCellWithThumbnail = document.createElement('td');
-        var newThumbnail = document.createElement('img');
-        newThumbnail.setAttribute('src', "".concat(users[i].picture.thumbnail));
+}
+const ageDistribution = {
+    '20-29': 0,
+    '30-39': 0,
+    '40-49': 0,
+    '50-59': 0,
+    '60-69': 0,
+    '70-79': 0,
+    '80-89': 0,
+    '90-99': 0,
+};
+function countUsersInRanges(users) {
+    users.forEach(user => {
+        if (user.dob.age >= 20 && user.dob.age < 30) {
+            ageDistribution['20-29']++;
+        }
+        if (user.dob.age >= 30 && user.dob.age < 40) {
+            ageDistribution['30-39']++;
+        }
+        if (user.dob.age >= 40 && user.dob.age < 50) {
+            ageDistribution['40-49']++;
+        }
+        if (user.dob.age >= 50 && user.dob.age < 60) {
+            ageDistribution['50-59']++;
+        }
+        if (user.dob.age >= 60 && user.dob.age < 70) {
+            ageDistribution['60-69']++;
+        }
+        if (user.dob.age >= 70 && user.dob.age < 80) {
+            ageDistribution['70-79']++;
+        }
+        if (user.dob.age >= 80 && user.dob.age < 90) {
+            ageDistribution['80-89']++;
+        }
+        if (user.dob.age >= 90 && user.dob.age < 100) {
+            ageDistribution['90-99']++;
+        }
+    });
+}
+const infoArr = ['Thumbnail', 'Age', 'Name', 'City'];
+function showTableHeader(infoArr) {
+    const tableHeader = document.getElementById('tableHeader');
+    const newTableRow = document.createElement('tr');
+    infoArr.forEach(info => {
+        const newHeader = document.createElement('th');
+        newHeader.innerHTML = info;
+        newTableRow.appendChild(newHeader);
+    });
+    tableHeader.appendChild(newTableRow);
+}
+function showTable(users) {
+    const tableResults = document.getElementById('tableResults');
+    showTableHeader(infoArr);
+    for (let i = 999; i >= 990; i--) {
+        const newTableRow = document.createElement('tr');
+        const newDataCellWithThumbnail = document.createElement('td');
+        const newThumbnail = document.createElement('img');
+        newThumbnail.setAttribute('src', `${users[i].picture.thumbnail}`);
         newThumbnail.setAttribute('class', 'thumbnail');
         newDataCellWithThumbnail.appendChild(newThumbnail);
         newTableRow.appendChild(newDataCellWithThumbnail);
-        var newDataCellWithAge = document.createElement('td');
+        const newDataCellWithAge = document.createElement('td');
         newDataCellWithAge.innerHTML = users[i].dob.age.toString();
         newTableRow.appendChild(newDataCellWithAge);
-        var newDataCellWithName = document.createElement('td');
-        newDataCellWithName.innerHTML = "".concat(users[i].name.first, " ").concat(users[i].name.last);
+        const newDataCellWithName = document.createElement('td');
+        newDataCellWithName.innerHTML = `${users[i].name.first} ${users[i].name.last}`;
         newTableRow.appendChild(newDataCellWithName);
-        var newDataCellWithCity = document.createElement('td');
+        const newDataCellWithCity = document.createElement('td');
         newDataCellWithCity.innerHTML = users[i].location.city.toString();
         newTableRow.appendChild(newDataCellWithCity);
         tableResults.appendChild(newTableRow);
     }
-});
+}
+function showChart(users) {
+    const labels = [];
+    const amountInRange = [];
+    for (const range in ageDistribution) {
+        labels.push(range);
+        amountInRange.push(ageDistribution[range]);
+    }
+    const chartData = {
+        labels: labels,
+        datasets: [
+            {
+                label: 'Distribution of French Men Age',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: amountInRange,
+            },
+        ],
+    };
+    const config = {
+        type: 'bar',
+        data: chartData,
+        options: {
+            responsive: true,
+        },
+    };
+    const myChart = new Chart(document.getElementById('myChart'), config);
+}
+function showLoadingIndicator() {
+    const loadingIndicator = document.getElementById('loadingIndicator');
+    loadingIndicator === null || loadingIndicator === void 0 ? void 0 : loadingIndicator.setAttribute('class', 'visible');
+}
+function hideLoadingIndicator() {
+    const loadingIndicator = document.getElementById('loadingIndicator');
+    loadingIndicator === null || loadingIndicator === void 0 ? void 0 : loadingIndicator.setAttribute('class', 'hidden');
+}
+const chartTitle = document.getElementById('chartTitle');
+const tableTitle = document.getElementById('tableTitle');
+function showTitles(chartTitle, tableTitle) {
+    chartTitle.setAttribute('class', 'visible');
+    tableTitle.setAttribute('class', 'visible');
+}
+function loadData() {
+    return getUsers().then((users) => {
+        sortByAge(users);
+        countUsersInRanges(users);
+        hideLoadingIndicator();
+        showTitles(chartTitle, tableTitle);
+        showChart(users);
+        showTable(users);
+    });
+}
+const onClickHandler = () => {
+    showLoadingIndicator();
+    loadData();
+    console.log('clicked');
+};
--- app.ts
+++ app.ts
@@ -34,9 +34,6 @@ interface User {
   };
 }
 
-// let frenchMenData: Array<FrenchMen>;
-// let frenchMenAges: Array<number> = [];
-
 function getUsers(): Promise<Array<User>> {
   return fetch(
     'https://randomuser.me/api/?results=1000&gender=male&nat=fr&inc=name,location,dob,picture&noinfo'
@@ -44,22 +41,8 @@ function getUsers(): Promise<Array<User>> {
     .then((res) => res.json())
     .then((res) => {
       return res.results as Array<User>;
-      // frenchMenData = res.results;
-
-      // let frenchMen: FrenchMen;
-      // for (let frenchMen in frenchMenData) {
-      //     frenchMenAges.push((frenchMen.dob.age);
-      //     console.log(`${frenchMenData[0].name.first} ${frenchMenData[0].name.last}, age: ${frenchMenData[0].dob.age}, city: ${frenchMenData[0].location.city}`)
-      // }
     });
 }
-
-// const result: HTMLElement | null = document.getElementById('result');
-// const myResults = getUsers().then((users) => {
-//   for (let user in users) {
-//       console.log(user);
-//   }
-// });
 
 function compareAge(a, b) {
   if (a.dob.age < b.dob.age) {
@@ -71,9 +54,67 @@ function compareAge(a, b) {
   return 0;
 }
 
-const tableResults = document.getElementById('tableResults');
-getUsers().then((users) => {
+function sortByAge(users) {
   users.sort(compareAge);
+}
+
+const ageDistribution = {
+  '20-29': 0,
+  '30-39': 0,
+  '40-49': 0,
+  '50-59': 0,
+  '60-69': 0,
+  '70-79': 0,
+  '80-89': 0,
+  '90-99': 0,
+};
+
+function countUsersInRanges(users) {
+  users.forEach(user => {
+    if (user.dob.age >= 20 && user.dob.age < 30) {
+      ageDistribution['20-29']++;
+    }
+    if (user.dob.age >= 30 && user.dob.age < 40) {
+      ageDistribution['30-39']++;
+    }
+    if (user.dob.age >= 40 && user.dob.age < 50) {
+      ageDistribution['40-49']++;
+    }
+    if (user.dob.age >= 50 && user.dob.age < 60) {
+      ageDistribution['50-59']++;
+    }
+    if (user.dob.age >= 60 && user.dob.age < 70) {
+      ageDistribution['60-69']++;
+    }
+    if (user.dob.age >= 70 && user.dob.age < 80) {
+      ageDistribution['70-79']++;
+    }
+    if (user.dob.age >= 80 && user.dob.age < 90) {
+      ageDistribution['80-89']++;
+    }
+    if (user.dob.age >= 90 && user.dob.age < 100) {
+      ageDistribution['90-99']++;
+    }
+  })
+}
+
+const infoArr: Array<string> = ['Thumbnail', 'Age', 'Name', 'City'];
+
+function showTableHeader(infoArr: Array<string>) {
+  const tableHeader = document.getElementById('tableHeader')!;
+  const newTableRow = document.createElement('tr');
+  infoArr.forEach(info => {
+    const newHeader = document.createElement('th');
+    newHeader.innerHTML = info;
+    newTableRow.appendChild(newHeader);
+  });
+  tableHeader.appendChild(newTableRow);
+  
+}
+
+function showTable(users) {
+  const tableResults = document.getElementById('tableResults');
+  showTableHeader(infoArr);
   for (let i = 999; i >= 990; i--) {
     const newTableRow = document.createElement('tr');
 
@@ -99,4 +140,76 @@ getUsers().then((users) => {
 
     tableResults!.appendChild(newTableRow);
   }
-});
+}
+
+function showChart(users) {
+
+  const labels: Array<string> = [];
+  const amountInRange: Array<number> = [];
+
+  for (const range in ageDistribution) {
+    labels.push(range);
+    amountInRange.push(ageDistribution[range]);
+  }
+
+  const chartData = {
+    labels: labels,
+    datasets: [
+      {
+        label: 'Distribution of French Men Age',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: amountInRange,
+      },
+    ],
+  };
+
+  const config = {
+    type: 'bar',
+    data: chartData,
+    options: {
+      responsive: true,
+    },
+  };
+
+  const myChart: Chart = new Chart(
+    document.getElementById('myChart') as HTMLCanvasElement,
+    config
+  );
+}
+
+function showLoadingIndicator() {
+  const loadingIndicator = document.getElementById('loadingIndicator');
+  loadingIndicator?.setAttribute('class', 'visible');
+}
+
+function hideLoadingIndicator() {
+  const loadingIndicator = document.getElementById('loadingIndicator');
+  loadingIndicator?.setAttribute('class', 'hidden');
+}
+
+const chartTitle = document.getElementById('chartTitle');
+const tableTitle = document.getElementById('tableTitle');
+
+
+function showTitles(chartTitle, tableTitle) {
+  chartTitle.setAttribute('class', 'visible');
+  tableTitle.setAttribute('class', 'visible');
+}
+
+function loadData() {
+  return getUsers().then((users) => {
+    sortByAge(users);
+    countUsersInRanges(users);
+    hideLoadingIndicator();
+    showTitles(chartTitle, tableTitle);
+    showChart(users);
+    showTable(users);
+  });
+}
+
+const onClickHandler = () => {
+  showLoadingIndicator();
+  loadData();
+  console.log('clicked');
+};
